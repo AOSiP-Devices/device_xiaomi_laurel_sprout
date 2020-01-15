6408f6c405e793c9c976fe272b15b420b160633e
David Sn <divad.nnamtdeis@gmail.com>
2020-01-15 02:11:14 +0100

    [DNM] AOSiP-ify AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_laurel_sprout.mk
+    $(LOCAL_DIR)/aosip_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_laurel_sprout-userdebug
+    aosip_laurel_sprout-userdebug
