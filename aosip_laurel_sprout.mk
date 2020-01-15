b4db17eed91282bdc2a826d8cbaf80baf10e8f31
David Sn <divad.nnamtdeis@gmail.com>
2020-01-15 02:07:20 +0100

    Rename to aosip_laurel_sprout.mk and AOSiP-ify

---
+++ aosip_laurel_sprout.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from laurel_sprout device
+$(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosip_laurel_sprout
+PRODUCT_DEVICE := laurel_sprout
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=laurel_sprout \
+    PRODUCT_NAME=laurel_sprout \
+    PRIVATE_BUILD_DESC="laurel_sprout-user 9 PKQ1.190416.001 V10.3.6.0.PFQMIXM release-keys"
+
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys"
